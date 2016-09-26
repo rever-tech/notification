1560594b287190610fef86fc96b156ba597f5d64
PhuongLT <phuong@rever.vn>
2016-09-26 16:50:05 +0700

    Update server & controller

--- src/main/scala/ms/notification/Server.scala
+++ src/main/scala/ms/notification/Server.scala
@@ -6,22 +6,24 @@ import com.twitter.finatra.http.filters.CommonFilters
 import com.twitter.finatra.http.routing.HttpRouter
 import com.twitter.finatra.thrift.ThriftServer
 import com.twitter.finatra.thrift.routing.ThriftRouter
+import ms.notification.controller.{http, thrift}
 import ms.notification.module.NotificationModule
 import ms.notification.util.ZConfig
-import ms.notification.controller.http
 
 /**
-  * Created by SangDang on 9/8/
-  **/
+ * Created by SangDang on 9/8/
+ **/
 object MainApp extends Server
+
 class Server extends HttpServer with ThriftServer {
 
-  override protected def defaultFinatraHttpPort: String = ":" + ZConfig.getString("server.http.port","8001")
+  override protected def defaultFinatraHttpPort: String = ":" + ZConfig.getString("server.http.port", "8001")
 
-  override protected def defaultFinatraThriftPort: String = ZConfig.getString("server.thrift.port",":9001")
+  override protected def defaultFinatraThriftPort: String = ZConfig.getString("server.thrift.port", ":9001")
 
   override val modules = Seq(NotificationModule)
-  override protected def disableAdminHttpServer: Boolean = ZConfig.getBoolean("server.admin.disable",true)
+
+  override protected def disableAdminHttpServer: Boolean = ZConfig.getBoolean("server.admin.disable", default = true)
 
   override protected def configureHttp(router: HttpRouter): Unit = {
     router.filter[CommonFilters]
@@ -29,7 +31,7 @@ class Server extends HttpServer with ThriftServer {
   }
 
   override protected def configureThrift(router: ThriftRouter): Unit = {
-//    router
-//      .add[thrift.NotificationController]
+    router
+      .add[thrift.NotificationController]
   }
 }
